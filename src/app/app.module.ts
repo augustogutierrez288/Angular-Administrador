42b6077828cfe4e9b7cd11280c97819313f2a84d
Augusto Gutierrez <augustogutierrez288@gmail.com>
2023-07-10 17:32:44 -0300

    create component main, aside, navbar and write basic structure of html5 and your style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { AsideComponent } from './aside/aside.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    AsideComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
